9406996fd07970af52500a17d0dfa2ad9c6faec6
Guðmundur Brimir Björnsson <gudmundurbj296@nemi.tskoli.is>
2020-01-24 08:36:22 +0000

    Skilaverkefni 1 er klárað og sett upp á github skýinu.

---
+++ Verkefni_1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    public void Endgame() {
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            // Restart Game
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
